d4b0e71283a21f6634428fce8a88069d5794a417
rcupull <59990075+rcupull@users.noreply.github.com>
2024-04-24 12:29:23 -0400

    update

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,1 +1,2 @@
-export const SECRET_ACCESS_TOKEN = "23f9e696-c964-4201-a40f-b7361a872537"; //TODO move to env
+export const SECRET_ACCESS_TOKEN =
+  process.env.SECRET_ACCESS_TOKEN || "23f9e696-c964-4201-a40f-b7361a872537";
