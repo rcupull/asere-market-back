0f1d25abefa7ecd50d7a358cd30924956f4808e3
rcupull <59990075+rcupull@users.noreply.github.com>
2024-04-23 21:43:48 -0400

    update

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,25 @@
 import { connect } from "mongoose";
 
-// type dbName = "elocality_auth";
+const NODE_ENV = process.env.NODE_ENV;
+const ATLAS_PASS = process.env.ATLAS_PASS;
 
-// const getUri = (db: dbName) => {
-//   return `mongodb+srv://rcupull:Alcatraz-32286-elocality-db@cluster0.3ageacp.mongodb.net/${db}?retryWrites=true&w=majority`;
-// };
+const getAtlasUrlDb = (db: string) => {
+  return `mongodb+srv://rcupull:${ATLAS_PASS}@cluster0.3ageacp.mongodb.net/${db}?retryWrites=true&w=majority`;
+};
+const getUrl = () => {
+  if (NODE_ENV === "production") {
+    return getAtlasUrlDb("asere-market-prod");
+  }
 
-// const elocalityAuthUri = getUri("elocality_auth");
+  if (NODE_ENV === "staging") {
+    return getAtlasUrlDb("asere-market-stage");
+  }
 
-export const dbUrl = "mongodb://127.0.0.1:27017/community_db";
+  return "mongodb://127.0.0.1:27017/community_db";
+};
 
 export const connectDB = () => {
-  connect(dbUrl)
+  connect(getUrl())
     .then(() => {
       console.log("connected");
     })
